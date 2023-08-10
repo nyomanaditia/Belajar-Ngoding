045df5a36d655abc91f7fe61515a5f0066051758
Mansadhit301 <aadhit407@gmail.com>
2023-08-10 09:59:47 +0800

    modifikasi soal fpb

--- minggu3/fpb.js
+++ minggu3/fpb.js
@@ -1,41 +1,11 @@
 //cari faktor persekutuan terbesar
-function fpb(angka1, angka2) {
-  // you can only write your code here!
-  let faktor1 = [];
-  let faktor2 = [];
-  let faktorSama = [];
-
-  //mencari faktor-faktornya
-  for(let i = 0; i <= angka1; i++) {
-    if(angka1 % i == 0) {
-      faktor1.push(i);
+function fpb(a, b) {
+  for(let i = a + b; i > 0 ; i--) {
+    if(a % i == 0 && b % i == 0) {
+      return i;
     }
   }
-  for(let i = 0; i <= angka2; i++) {
-    if(angka2 % i == 0) {
-      faktor2.push(i);
-    }
-  }
-
-  //mengelompokan nilai yang sama
-  for(let i = 0; i < faktor1.length; i++) {
-    for(let j = 0; j < faktor2.length; j++) {
-      if(faktor1[i] == faktor2[j]) {
-        faktorSama.push(faktor2[j]);
-      }
-    }
-  }
-  
-  //mencari fpb yang terbesar
-  let fpb = 0;
-  for(let i = 0; i < faktorSama.length; i++) {
-    if(faktorSama[i] > fpb) {
-      fpb = faktorSama[i];
-    }
-  }
-  return fpb;
 }
-
 // TEST CASES
 console.log(fpb(12, 16)); // 4
 console.log(fpb(50, 40)); // 10
